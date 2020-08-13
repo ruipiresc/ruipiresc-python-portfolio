bb7ddabd0529aeb3b90b08c542f182ee0940ce29
Rui Pires <ruipiresc@gmail.com>
2020-08-13 17:43:28 +0100

    completed update holdings

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -2,28 +2,22 @@ import datetime as dt
 from typing import Any
 import wraplite as wl
 import pandas as pd
+from .holding import Holding
+from .services import alpha_vantage, iex_cloud, yahoo_finance
 from . import gmail, trading212, utils
 
 class Portfolio(object):
   """Your portfolio class."""
 
   database_name: str = 'portfolio'
-  _db = None
-  _gmail = None
+  base_currency: str = 'EUR'
+  _db: wl.Database = None
+  _gmail: Any = None
 
-  @property
-  def db(self) -> wl.Database:
-    if self._db == None:
-      self._db = wl.get(self.database_name)
-    return self._db
+  def __init__(self) -> None:
+    self.gmail
 
-  @property
-  def gmail(self) -> Any:
-    if self._gmail == None:
-      self._gmail = gmail.get_service()
-    return self._gmail
-
-  def set_up(self) -> str:
+  def setup(self) -> str:
     self.db.create_table('brokers', wl.table.TableFormat(
       id= str,
       name= str,
@@ -58,60 +52,97 @@ class Portfolio(object):
       timestamp= dt.datetime,
       statement_id= str
     ).primary_keys(['id']))
-
-    self.db.create_table('symbols', wl.table.TableFormat(
+  
+    self.db.create_table('holdings', wl.table.TableFormat(
       id= str,
       symbol= str,
       exchange= str,
-      last_price= str,
-      timestamp= float,
-      sector= str,
-      industry= float,
-    ).primary_keys(['id']))
+      currency= str,
+      quantity= float,
+      price= float,
+      market_value= float,
+      price_native= float,
+      market_value_native= float,
+      exchange_rate= float,
+      timestamp= dt.datetime
+    ).primary_keys(['id', 'timestamp']))
 
-    self.gmail
-    return 'tables and connection to gmail ok. Set up complete'
+    self.data.setup()
+    return 'setup completed'
+
+  @property
+  def db(self) -> wl.Database:
+    if self._db == None:
+      self._db = wl.get(self.database_name)
+    return self._db
+
+  @property
+  def gmail(self) -> Any:
+    if self._gmail == None:
+      self._gmail = gmail.get_service()
+    return self._gmail
 
   def update(self) -> str:
     def update_trades() -> None:
       email_ids = gmail.search_messages(service = self.gmail, user_id = 'me', query = trading212.build_gmail_query())
       for email_id in email_ids:
-        if len(self.db.statements.query(f'SELECT * FROM statements WHERE email_id=\'{email_id}\'')) == 0:
+        if len(self.db.statements.query(f"SELECT * FROM statements WHERE email_id='{email_id}'")) == 0:
           email = gmail.get_message(service = self.gmail, user_id = 'me', msg_id = email_id)
           broker, statement, trades = trading212.process_email_statement(email, email_id)
-          if len(self.db.brokers.query(f'SELECT * FROM brokers WHERE id=\'{broker.id}\'')) == 0:
+          if len(self.db.brokers.query(f"SELECT * FROM brokers WHERE id='{broker.id}'")) == 0:
             self.db.brokers.insert(utils.to_df(broker))
             print(f'added broker: {broker.name}')
           self.db.statements.insert(utils.to_df(statement))
           money_invested = sum(trade.total_cost for trade in trades)
           print(f'processed statement from "{broker.name}" of <{statement.date}> with #{len(trades)} trades and {money_invested} {broker.currency} invested')
           self.db.trades.insert(utils.to_df(trades))
-    def update_symbols() -> None:
-      raise NotImplementedError()
+    def update_holdings() -> None:
+      trades: pd.DataFrame = self.db.trades.get_all()
+      holdings = []
+      for symbol in trades.symbol.unique():
+        symbol_trades = trades[trades['symbol']==symbol]
+        stock_fundamentals = yahoo_finance.get_fundamentals(symbol)
+        holdings.append(Holding(
+          symbol= symbol,
+          currency= stock_fundamentals['currency'],
+          exchange= stock_fundamentals['exchange'],
+          quantity= symbol_trades.quantity.sum(),
+          exchange_rate= yahoo_finance.get_exchange_rate(stock_fundamentals['currency'], self.base_currency),
+          price_native= yahoo_finance.get_last_price(symbol),
+        ))
+      self.db.holdings.insert(utils.to_df(holdings))
     update_trades()
-    update_symbols()
+    update_holdings()
     return 'portfolio trades and tickers updated'
-
-  def trades(self) -> str:
-    return str(self.db.trades.get_all())
-  
-  def statements(self) -> str:
-    return str(self.db.statements.get_all())
-
-  def brokers(self) -> str:
-    return str(self.db.brokers.get_all())
 
   def money_invested(self) -> str:
     results = []
     brokers: pd.Dataframe = self.db.brokers.get_all()
     for _, broker in brokers.iterrows():
       money_invested = 0.0
-      statements = self.db.statements.query(f'SELECT * FROM statements WHERE broker_id=\'{broker.id}\'')
+      statements = self.db.statements.query(f"SELECT * FROM statements WHERE broker_id='{broker.id}'")
       for _, statement in statements.iterrows():
-        trades = self.db.trades.query(f'SELECT * FROM trades WHERE statement_id=\'{statement.id}\'')
+        trades = self.db.trades.query(f"SELECT * FROM trades WHERE statement_id='{statement.id}'")
         money_invested += trades.total_cost.sum()
       results.append('you have {} {} invested on {}'.format(money_invested, broker.currency, broker['name']))
     return results
+  
+  def print(self, table_name: str) -> str:
+    if hasattr(self.db, table_name):
+      return str(getattr(self.db, table_name))
+    if hasattr(self.data.db, table_name):
+      return str(getattr(self.data.db, table_name))
+    return 'nothing to print'
+  
+  def drop(self, table_name: str) -> str:
+    result = []
+    if hasattr(self.db, table_name):
+      getattr(self.db, table_name).drop()
+      result.append(f'dropped table {table_name} from portfolio db')
+    if hasattr(self.data.db, table_name):
+      getattr(self.data.db, table_name).drop()
+      result.append(f'dropped table {table_name} from iex db')
+    return result
 
   def dump(self, table_name: str) -> str:
     table: wl.Table = getattr(self.db, table_name)
