74de1592f56acbc86912399e3fd566a76fe1bf50
Rui Pires <ruipiresc@gmail.com>
2020-08-13 17:43:45 +0100

    Bump version: 0.1.0.dev0 → 0.1.0.dev1

--- portfolio/__version__.py
+++ portfolio/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'portfolio'
 __description__ = 'A python cli tool to show stats on your trades and portfolio'
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0.dev1'
 __author__ = 'Rui Pires'
 __author_email__ = 'ruipiresc@gmail.com'
 __license__ = 'GPLv3'
